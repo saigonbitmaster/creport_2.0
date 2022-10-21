2cbfb87885cecf6164e104dd627e2cf85f6d479c
saigonbitmaster <bitmaster.sg@gmail.com>
2022-10-22 05:15:47 +0700

    proposal kpi reports

---
+++ cms/src/kpi/list.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  FunctionField,
+  ReferenceField,
+  SearchInput,
+  ReferenceInput,
+  SelectInput,
+  SelectField,
+} from "react-admin";
+import { Link } from "react-router-dom";
+import ColoredNumberField from "../components/currencyNumberField";
+import ProposalRateField from "../components/rateProposal";
+import { choices } from "../data/data";
+
+const ListScreen = () => {
+  const Filters = [
+    <SearchInput source="name" alwaysOn />,
+    <ReferenceInput source="fundId" reference="funds" required alwaysOn>
+      <SelectInput optionText="name" fullWidth />
+    </ReferenceInput>,
+    <ReferenceInput source="proposerId" reference="proposers" required alwaysOn>
+      <SelectInput optionText="fullName" fullWidth />
+    </ReferenceInput>,
+  ];
+
+  return (
+    <List
+      perPage={25}
+      sort={{ field: "date", order: "desc" }}
+      hasCreate={false}
+      filters={Filters}
+    >
+      <Datagrid bulkActionButtons={false}>
+        <TextField source="name" />
+        <ProposalRateField label="Rate" />
+        <TextField source="projectId" />
+        <SelectField source="projectStatus" choices={choices} />
+        <ReferenceField source="fundId" reference="funds">
+          <TextField source="name" />
+        </ReferenceField>
+
+        <FunctionField
+          label="Git Commits"
+          render={(record) => (
+            <Link
+              to={`/commits?filter=${JSON.stringify({
+                proposalId: record.id,
+              })}`}
+            >
+              {record.commits}
+            </Link>
+          )}
+        />
+        <ReferenceField source="challengeId" reference="challenges">
+          <TextField source="name" />
+        </ReferenceField>
+        <ReferenceField source="proposerId" reference="proposers">
+          <TextField source="fullName" />
+        </ReferenceField>
+        <ColoredNumberField source="requestedBudget" threshold={50000} />
+      </Datagrid>
+    </List>
+  );
+};
+
+export default ListScreen;
